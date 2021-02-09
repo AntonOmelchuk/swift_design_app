eb6997c1c45cb1a2f4e40f1288aee0a252a6534b
Anton Omelchuk <kino_kg@ukr.net>
2021-02-09 23:24:16 +0200

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Anton Omelchuk on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
