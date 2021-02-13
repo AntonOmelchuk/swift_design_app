9b316de18b6182e9c3477f6f5cf2e7fec126a140
Anton Omelchuk <kino_kg@ukr.net>
2021-02-13 23:09:21 +0200

    add drawer

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -7,23 +7,16 @@ struct Home_Previews: PreviewProvider {
 }
 
 struct Home: View {
-    var menuItems = ["My Account", "Billing", "Team", "Sign out"]
-    var menu = menuData
-    
+    @State var show = false
+
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            ForEach(menu) { item in
-                MenuRow(image: item.icon, label: item.label)
-            }
-            Spacer()
+        ZStack {
+            Button(action: { self.show.toggle() }, label: {
+                Text("Open Menu")
+            })
+            
+            MenuView(show: $show)
         }
-        .padding(.top, 20)
-        .padding(30)
-        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-        .background(Color.white)
-        .cornerRadius(30)
-        .padding(.trailing, 60)
-        .shadow(radius: 21)
     }
 }
 
@@ -56,3 +49,31 @@ let menuData = [
     MenuInterface(icon: "person.and.person", label: "Team"),
     MenuInterface(icon: "arrow.uturn.down", label: "Sign out"),
 ]
+
+struct MenuView: View {
+    var menuItems = ["My Account", "Billing", "Team", "Sign out"]
+    var menu = menuData
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ForEach(menu) { item in
+                MenuRow(image: item.icon, label: item.label)
+            }
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 21)
+        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
+        .animation(.default)
+        .offset(x: show ? 0 : -UIScreen.main.bounds.width)
+        .onTapGesture {
+            self.show.toggle()
+        }
+    }
+}
