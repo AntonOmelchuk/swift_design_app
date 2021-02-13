b75f1e1cbc207ad48e2e90db907e322341e37fab
Anton Omelchuk <kino_kg@ukr.net>
2021-02-13 22:44:37 +0200

    add Home screen with init menu

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -1,20 +1,43 @@
-//
-//  Home.swift
-//  DesignCode
-//
-//  Created by Anton Omelchuk on 13.02.2021.
-//
-
 import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            MenuRow(image: "person.crop.circle", label: "My Account")
+            MenuRow(image: "creditcard", label: "Billing")
+            MenuRow(image: "person.and.person", label: "Team")
+            MenuRow(image: "arrow.uturn.down", label: "Sign out")
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 21)
     }
 }
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+    }
+}
+
+struct MenuRow: View {
+    var image = "creditcard"
+    var label = "My Account"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color(.black))
+                .frame(width: 32, height: 32)
+            Text(label)
+                .font(.headline)
+            Spacer()
+        }
     }
 }
