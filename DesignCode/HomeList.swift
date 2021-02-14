8eddff9ee245b087b73756f7064d175a1ed3b754
Anton Omelchuk <kino_kg@ukr.net>
2021-02-14 18:41:21 +0200

    add course data, scroll list

--- DesignCode/HomeList.swift
+++ DesignCode/HomeList.swift
@@ -1,13 +1,27 @@
 import SwiftUI
 
 struct HomeList: View {
+    @State var show = false
+    var courses = coursesData
+    
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                ForEach(0 ..< 3) { item in
-                    CourseView()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(courses) { item in
+                    Button(action: { self.show.toggle() }) {
+                        CourseView(
+                            title: item.title,
+                            image: item.image,
+                            color: item.color,
+                            shadowColor: item.shadowColor
+                        )
+                    }
+                    
                 }
-            }
+            }.sheet(isPresented: $show, content: {
+                Home()
+            })
+            .padding(.leading, 30)
         }
             
     }
@@ -20,21 +34,44 @@ struct HomeList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+    var title = "Build an app with SwiftUI"
+    var image = "Illustration1"
+    var color = Color("background3")
+    var shadowColor = Color("backgroundShadow3")
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Build an app with SwiftUI")
+            Text(title)
                 .font(.title)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 .foregroundColor(Color.white)
                 .padding(20)
                 .lineLimit(4)
-                .frame(width: 160)
+                .padding(.trailing, 30)
             Spacer()
-            Image("Illustration1")
+            Image(image)
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 246, height: 150)
+                .padding(.bottom, 30)
         }
-        .background(Color("background3"))
+        .background(color)
         .cornerRadius(30)
         .frame(width: 246, height: 360)
-        .shadow(color: Color("backgroundShadow3"), radius: 25, x: 0, y: 20)
+        .shadow(color: shadowColor, radius: 25, x: 0, y: 20)
     }
 }
+
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var color: Color
+    var shadowColor: Color
+}
+
+let coursesData = [
+    Course(title: "Build an app with SwiftUI", image: "Illustration1", color: Color("background3"), shadowColor: Color("backgroundShadow3")),
+    Course(title: "Design Course", image: "Illustration2", color: Color("background4"), shadowColor: Color("backgroundShadow4")),
+]
